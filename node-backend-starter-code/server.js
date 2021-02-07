6948f9d344cfe11573141a7391475129a9444cae
Sayf Ismail <sayfti@gmail.com>
2021-02-07 19:49:54 +0800

    Corrected errors in original file. Server now launches.

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -2,24 +2,25 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser')
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
-app.get('favorites', function(req, res){
+app.get('/favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
-  
+  }
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
@@ -27,6 +28,6 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
-  console.log("Listening on port 3000");
-});
+app.listen(3000, function(){
+  console.log("Listening on port 3000")
+})
